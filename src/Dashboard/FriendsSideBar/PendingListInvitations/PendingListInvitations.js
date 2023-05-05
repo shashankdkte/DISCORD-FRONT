bd3431d6f5059c74af1d5520de81e07b8faf5556
Shashank Dhakate <shashankdkte@gmail.com>
2023-05-05 16:50:10 +0530

    Creating Dummy Invitations List

--- src/Dashboard/FriendsSideBar/PendingListInvitations/PendingListInvitations.js
+++ src/Dashboard/FriendsSideBar/PendingListInvitations/PendingListInvitations.js
@@ -1,5 +1,36 @@
 import React from "react";
 import { styled } from "@mui/system";
+import PendingInvitationListItem from "./PendingInvitationListItem";
+const DUMMY_INVATIONS = [
+  {
+    _id: "1",
+    senderId: {
+      username: "Harry",
+      mail: "harry@dg.com",
+    },
+  },
+  {
+    _id: "2",
+    senderId: {
+      username: "Polly",
+      mail: "polly@dg.com",
+    },
+  },
+  // {
+  //   _id: "3",
+  //   senderId: {
+  //     username: "JugHead",
+  //     mail: "Jug@dg.com",
+  //   },
+  // },
+  // {
+  //   _id: "4",
+  //   senderId: {
+  //     username: "Vannesa",
+  //     mail: "vans@dg.com",
+  //   },
+  // },
+];
 const MainContainer = styled("div")({
   width: "100%",
   height: "22%",
@@ -10,7 +41,18 @@ const MainContainer = styled("div")({
 });
 
 const PendingListInvitations = () => {
-  return <MainContainer></MainContainer>;
+  return (
+    <MainContainer>
+      {DUMMY_INVATIONS.map((invitation) => (
+        <PendingInvitationListItem
+          key={invitation._id}
+          id={invitation._id}
+          username={invitation.senderId.username}
+          mail={invitation.senderId.mail}
+        />
+      ))}
+    </MainContainer>
+  );
 };
 
 export default PendingListInvitations;
